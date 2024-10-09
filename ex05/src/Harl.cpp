122c174bfa2d9a0b73c1122f93a20c98c0245f03
Kudo Tomoya <ktomoya@c4r2s16.42tokyo.jp>
2024-10-09 22:04:21 +0900

    fix: change error message

--- ex05/src/Harl.cpp
+++ ex05/src/Harl.cpp
@@ -18,17 +18,27 @@ void Harl::complain(std::string level) {
 }
 
 void Harl::debug() {
-  std::cout << "debug" << std::endl;
+  std::cout << "[ DEBUG ]" << std::endl
+            << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger." << std::endl
+            << "I really do!" << std::endl << std::endl;
 }
 
 void Harl::info() {
-  std::cout << "info" << std::endl;
+  std::cout << "[ INFO ]" << std::endl
+            << "I cannot believe adding extra bacon costs more money." << std::endl
+            << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
+            << std::endl << std::endl;
 }
 
 void Harl::warning() {
-  std::cout << "warning" << std::endl;
+  std::cout << "[ WARNING ]" << std::endl
+            << "I think I deserve to have some extra bacon for free." << std::endl
+            << "I’ve been coming for years whereas you started working here since last month."
+            << std::endl << std::endl;
 }
 
 void Harl::error() {
-  std::cout << "error" << std::endl;
+  std::cout << "[ ERROR ]" << std::endl
+            << "This is unacceptable! I want to speak to the manager now."
+            << std::endl << std::endl;
 }
