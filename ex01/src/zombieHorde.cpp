17b364a619e408c1b8d3fa03f9d92658ea6883cd
tom <134240277+ktomoya1@users.noreply.github.com>
2024-10-12 15:21:12 +0900

    fix: return NULL if the integer is negative

--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -1,7 +1,12 @@
 #include "Zombie.h"
 
 Zombie* zombieHorde(int N, std::string name) {
+  if (N < 0) {
+    return NULL;
+  }
+
   Zombie* zombies = new Zombie[N];
+
   for (int i = 0; i < N; ++i) {
     zombies[i].setName(name);
   }
